0ceea4c5db4e6c65fdcd8cf373b8adfaaf11ebd5
Stephen Novelli <snovelli1021@gmail.com>
2022-10-09 14:11:30 -0700

    Added file structure and copied boiler plate code with tutor for models, views, controllers, from class assignments.

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,65 @@
+const router = require("express").Router();
+const { User } = require("../../models");
+
+router.post("/", async (req, res) => {
+  try {
+    const user = await User.create({
+      email: req.body.email,
+      password: req.body.password,
+    });
+    req.session.save(() => {
+      req.session.user_id = user.id;
+      req.session.logged_in = true;
+      res.json({ user: user, message: "Your user account is now created!" });
+    });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ message: "Invalid email or password, please try again" });
+    return;
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { email: req.body.email } });
+
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: "Incorrect email or password, please try again" });
+      return;
+    }
+
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: "Incorrect email or password, please try again" });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.json({ user: userData, message: "You are now logged in!" });
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.post("/logout", (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
+module.exports = router;
