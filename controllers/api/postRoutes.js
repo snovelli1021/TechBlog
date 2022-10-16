c09e78d680724e7087d76ee6ffa3a49ad57312cb
Stephen Novelli <snovelli1021@gmail.com>
2022-10-16 16:55:49 -0700

    Added routes, handlers, and placeholders in dashboard.handlebars for edit and delete post.

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const { Post } = require("../../models");
 const withAuth = require("../../utils/auth");
 
+//Creates a blog post
 router.post("/", withAuth, async (req, res) => {
   try {
     const post = await Post.create({
@@ -13,6 +14,54 @@ router.post("/", withAuth, async (req, res) => {
   } catch (error) {
     res.status(500).json({ message: "Your post failed to create." });
     return;
+  }
+});
+
+//Edits a blog post
+router.put("/:id", withAuth, (req, res) => {
+  Post.update(
+    {
+      postTitle: req.body.postTitle,
+      postBody: req.body.postBody,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((editPost) => {
+      if (!editPost) {
+        res
+          .status(404)
+          .json({ message: "Please select a post with a valid id" });
+        return;
+      }
+      res.json(editPost);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+//Deletes a blog post
+router.delete("/:id", withAuth, async (req, res) => {
+  try {
+    const deletePost = await Post.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!deletePost) {
+      res
+        .status(404)
+        .json({ message: "Please select a post with a valid Id." });
+      return;
+    }
+    res.status(200).json(deletePost);
+  } catch (error) {
+    res.status(500).json(err);
   }
 });
 
--- public/js/post.js
+++ public/js/post.js
@@ -1,3 +1,4 @@
+//Handler used to create a post
 const postFormHandler = async (event) => {
   event.preventDefault();
   console.log("hello");
@@ -22,3 +23,53 @@ const postFormHandler = async (event) => {
 document
   .querySelector(".post-form")
   .addEventListener("submit", postFormHandler);
+
+//Handler used to edit a post
+const editPostHandler = async (event) => {
+  event.preventDefault();
+
+  const postNumber = specificId.innerHTML;
+  const postTitle = document.querySelector("#post-title").value.trim();
+  const postBody = document.querySelector("#post-body").value.trim();
+
+  if (postTitle && postBody) {
+    const response = await fetch(`/api/posts/${postNumber}`, {
+      method: "PUT",
+      body: JSON.stringify({ postTitle, postBody }),
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+    });
+
+    if (response.ok) {
+      document.location.replace("/dashboard");
+    } else {
+      alert("Editing your post has failed.");
+    }
+  }
+};
+
+document.querySelector(".post-form").addEventListener("edit", editPostHandler);
+
+//Handler used to delete a post
+const deletePostHandler = async (event) => {
+  event.preventDefault();
+
+  const postNumber = specificId.innerHTML;
+
+  const response = await fetch(`/api/posts/${postNumber}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (response.ok) {
+    document.location.replace("/dashboard");
+  } else {
+    console.log(err);
+  }
+};
+
+document
+  .querySelector(".post-form")
+  .addEventListener("delete", deletePostHandler);
